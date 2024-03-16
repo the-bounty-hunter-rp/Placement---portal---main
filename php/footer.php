0f8764a65d833ae64d67f87bb2d1aea9b3c615d7
Rahul <rahul@gamil.com>
2024-03-16 09:28:38 +0530

    first commit

---
+++ php/footer.php
@@ -0,0 +1,46 @@
+<footer id="footer" class="footer">
+
+
+
+    <div class="footer-legal text-center">
+        <div class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
+
+            <div class="d-flex flex-column align-items-center align-items-lg-start">
+                <div class="copyright">
+                    &copy; Copyright <strong><span>2024</span></strong>. All Rights Reserved
+                </div>
+                <div class="credits">
+
+                    <a href="https://vignan.ac.in/blog/how-accurate-are-the-placements-at-vignan-university.php">PlacementCell</a>
+                </div>
+            </div>
+
+            <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
+                <a href="https://www.linkedin.com/in/rahul-kumar-93a220247/" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                <a href="https://twitter.com/RAHULKU50476033" class="twitter"><i class="bi bi-twitter"></i></a>
+                <a href="https://www.facebook.com/profile.php?id=100044185029221" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="https://www.instagram.com/rahul__patel__75/" class="instagram"><i class="bi bi-instagram"></i></a>
+                <!--
+                <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>-->
+                
+            </div>
+
+        </div>
+    </div>
+
+</footer>
+
+<a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
